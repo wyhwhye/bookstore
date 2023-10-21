1b6f7c6961de28cf6602f57ebfbf2e8c3063e99d
Han_Han <2088113262@qq.com>
2023-10-21 15:48:28 +0800

    注释if

--- fe/test/test_bench.py
+++ fe/test/test_bench.py
@@ -2,7 +2,8 @@ from fe.bench.run import run_bench
 
 
 def test_bench():
-    try:
-        run_bench()
-    except Exception as e:
-        assert 200 == 100, "test_bench过程出现异常"
+    run_bench()
+    # try:
+    #     run_bench()
+    # except Exception as e:
+    #     assert 200 == 100, "test_bench过程出现异常"
